e4c8437506df34046ffb2edba8709752a6c115d2
JaveriaBaloch <streetback666@gmail.com>
2021-09-09 18:39:43 +0500

    UI Design is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React,{Component} from "react";
-
+import "./css/style.css"
 class App extends React.Component{
     render(){
         return(
             <main>
                 <section>
                     <h1>Count Down Champ</h1>
+                    <hr/>
+                </section>
+                <section>
+                    <span>14 days</span>
+                    <span>30 hours</span>
+                    <span>15 minutes</span>
+                    <span>20 seconds</span>
+                </section>
+                <section className="flied">
+                    <input placeholder="New Deadline" type="datetime-local"/>
+                    <button>Sumbit</button>
                 </section>
             </main>
             )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom";
 import App from "./App"
-import "./css/style.css"
 ReactDOM.render(
     <div>
         <App/>
